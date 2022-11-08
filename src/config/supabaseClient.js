e02537463eab0262689ad0a941cbdff88ef7a301
IsiteYves <yvesisite@gmail.com>
2022-11-08 17:45:16 +0200

    feat: resolve firebase config variables

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,7 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
+require("dotenv").config();
 
+console.log(process.env);
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+
+console.log("supabaseUrl..", supabaseUrl, supabaseAnonKey);
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
